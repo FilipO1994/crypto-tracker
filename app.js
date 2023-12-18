070c4bf601caac23fcb30ef72576063b5ad1f9a0
Filip <fosinski1@wp.pl>
2023-12-19 00:59:41 +0100

    new nav with search option + burger nav with search option

--- app.js
+++ app.js
@@ -1,55 +1,105 @@
-
-
 async function fetchData() {
+	try {
+		const response = await fetch('https://api.coincap.io/v2/assets/')
+		const data = await response.json()
 
- 
-  try {
-    const response = await fetch('https://api.coincap.io/v2/assets/');
-    const data = await response.json();
-    console.log(data);
-
-    displayCryptocurrencies(data);
-  } catch (error) {
-    console.error('Błąd podczas pobierania danych:', error);
-  }
+		displayCryptocurrencies(data)
+	} catch (error) {
+		console.error('Błąd podczas pobierania danych:', error)
+	}
 }
-
 
 function displayCryptocurrencies(data) {
-  
-  const selectedCryptos = ['bitcoin', 'ethereum','binance-coin','xrp', 'cardano','chainlink','litecoin', 'stellar','vechain', 'the-graph','quant','algorand','kava','neo','eos', 'iota','chiliz' ,'pancakeswap'];
+	const selectedCryptos = [
+		'bitcoin',
+		'ethereum',
+		'binance-coin',
+		'xrp',
+		'cardano',
+		'chainlink',
+		'litecoin',
+		'stellar',
+		'vechain',
+		'the-graph',
+		'quant',
+		'algorand',
+		'kava',
+		'neo',
+		'eos',
+		'iota',
+		'chiliz',
+		'pancakeswap',
+	]
 
-  selectedCryptos.forEach(cryptoId => {
-    const cryptoData = data.data.find(item => item.id === cryptoId);
-    console.log(cryptoData);
+	selectedCryptos.forEach(cryptoId => {
+		const cryptoData = data.data.find(item => item.id === cryptoId)
 
-    if (cryptoData) {
-       const selectedContainer = document.querySelector('.selected-container')
-       const cryptoDiv = document.createElement('div');
-       cryptoDiv.classList.add('crypto-card');
-       cryptoDiv.innerHTML = `
-           <p>Name: ${cryptoData.name}</p>
-           <p>Symbol: ${cryptoData.symbol}</p>
-           <p>Price: ${parseFloat(cryptoData.priceUsd).toFixed(2)} USD</p>
-           <p>Rank: ${cryptoData.rank}</p>
-           <p>Change in 24h: ${parseFloat(cryptoData.changePercent24Hr).toFixed(2)}%</p>
-           <p>Market: ${(parseFloat(cryptoData.marketCapUsd)/1000000).toFixed(0)} MLN USD</p>
-       `;
-       
-       
-       selectedContainer.appendChild(cryptoDiv);
-       
-       
-       const changeIn24hElement = cryptoDiv.querySelector('p:nth-child(5)'); // 
-       
-       
-       const changeIn24hValue = parseFloat(cryptoData.changePercent24Hr);
-       
-      
-       changeIn24hElement.style.color = changeIn24hValue > 0 ? '#86efac' : '#f87171';
-    }
-  });
+		if (cryptoData) {
+			const selectedContainer = document.querySelector('.selected-container')
+			const cryptoDiv = document.createElement('div')
+			cryptoDiv.classList.add('crypto-card')
+			cryptoDiv.dataset.name = cryptoData.name.toLowerCase()
+
+			cryptoDiv.innerHTML = `
+                <p>Name: ${cryptoData.name}</p>
+                <p>Symbol: ${cryptoData.symbol}</p>
+                <p>Price: ${parseFloat(cryptoData.priceUsd).toFixed(2)} USD</p>
+                <p>Rank: ${cryptoData.rank}</p>
+                <p>Change in 24h: ${parseFloat(cryptoData.changePercent24Hr).toFixed(2)}%</p>
+                <p>Market: ${(parseFloat(cryptoData.marketCapUsd) / 1000000).toFixed(0)} MLN USD</p>
+            `
+
+			selectedContainer.appendChild(cryptoDiv)
+
+			const changeIn24hElement = cryptoDiv.querySelector('p:nth-child(5)')
+			const changeIn24hValue = parseFloat(cryptoData.changePercent24Hr)
+
+			changeIn24hElement.style.color = changeIn24hValue > 0 ? '#86efac' : '#f87171'
+		}
+	})
 }
 
+const btn = document.querySelector('.search-btn')
+const input = document.querySelector('.search-input')
 
-fetchData();
+const openSearch = () => {
+	input.classList.toggle('active')
+}
+
+const searchToken = e => {
+	const searchValue = e.target.value.toLowerCase()
+	const cryptoDivs = document.querySelectorAll('.crypto-card')
+
+	cryptoDivs.forEach(cryptoDiv => {
+		const cryptoName = cryptoDiv.dataset.name
+
+		if (cryptoName.includes(searchValue)) {
+			cryptoDiv.classList.remove('hidden')
+		} else {
+			cryptoDiv.classList.add('hidden')
+		}
+	})
+}
+// burger menu opening
+const burgerMenu = document.querySelector('.burger-menu')
+const burgerBar = document.querySelector('.burger-bar')
+
+const toggleBurgerMenu = () => {
+	burgerMenu.classList.toggle('visable')
+}
+burgerBar.addEventListener('click', toggleBurgerMenu)
+
+// burger menu search
+const burgerSearch = document.querySelector('.burger-search-input')
+const burgerSearchBtn = document.querySelector('.burger-search-btn')
+const burgerInput = document.querySelector('.burger-search-input')
+
+const toggleBurgerSearch = () => {
+  burgerSearch.classList.toggle('active')
+}
+
+btn.addEventListener('click', openSearch)
+input.addEventListener('input', searchToken)
+burgerInput.addEventListener('input', searchToken)
+burgerSearchBtn.addEventListener('click',toggleBurgerSearch)
+fetchData()
