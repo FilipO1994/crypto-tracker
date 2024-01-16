fc3e434f1a62e946716f74c13a0eb76cba2195ec
Filip <fosinski1@wp.pl>
2024-01-16 23:04:41 +0100

    auto refresh API

--- gwei-tracker.js
+++ gwei-tracker.js
@@ -1,24 +1,33 @@
-const apiKey = 'BENNGHJVW5D83N7XWIET967RC3SBTDNNYY'
+const apiKey = 'BENNGHJVW5D83N7XWIET967RC3SBTDNNYY';
 
-const apiUrl = `https://api.etherscan.io/api?module=gastracker&action=gasoracle&apikey=${apiKey}`
+// Pobierz elementy HTML
+const standardElement = document.getElementsByClassName('standard-gas-fee')[0];
+const fastElement = document.getElementsByClassName('fast-gas-fee')[0];
+const instantElement = document.getElementsByClassName('instant-gas-fee')[0];
 
-fetch(apiUrl)
-	.then(response => response.json())
-	.then(data => {
-		if (data.status === '1') {
-			const standardGasPrice = data.result.SafeGasPrice
-			const fastGasPrice = data.result.ProposeGasPrice
-			const instantGasPrice = data.result.FastGasPrice
+// Funkcja do pobierania i aktualizacji cen gazu
+function fetchGasPrices() {
+    const apiUrl = `https://api.etherscan.io/api?module=gastracker&action=gasoracle&apikey=${apiKey}`;
 
-			standardElement.textContent = standardGasPrice
-			fastElement.textContent = fastGasPrice
-			instantElement.textContent = instantGasPrice
-		} else {
-			console.error('Błąd pobierania danych:', data.message)
-		}
-	})
-	.catch(error => console.error('Błąd pobierania danych:', error))
+    fetch(apiUrl)
+        .then(response => response.json())
+        .then(data => {
+            if (data.status === '1') {
+                const standardGasPrice = data.result.SafeGasPrice;
+                const fastGasPrice = data.result.ProposeGasPrice;
+                const instantGasPrice = data.result.FastGasPrice;
 
-const standardElement = document.getElementsByClassName('standard-gas-fee')[0]
-const fastElement = document.getElementsByClassName('fast-gas-fee')[0]
-const instantElement = document.getElementsByClassName('instant-gas-fee')[0]
+                // Przypisz pobrane ceny do odpowiednich elementów HTML
+                standardElement.textContent = standardGasPrice;
+                fastElement.textContent = fastGasPrice;
+                instantElement.textContent = instantGasPrice;
+            } else {
+                console.error('Błąd pobierania danych:', data.message);
+            }
+        })
+        .catch(error => console.error('Błąd pobierania danych:', error));
+}
+
+// Uruchom funkcję na początku i powtarzaj co pewien czas (np. co 30 sekund)
+fetchGasPrices();
+setInterval(fetchGasPrices, 30000);  // 30 000 ms = 30 s
