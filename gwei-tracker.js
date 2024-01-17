d4f09aa863e76e4debcac67fc957e7d39d0b9a4f
Filip <fosinski1@wp.pl>
2024-01-17 13:24:00 +0100

    custom alert gas fee added

--- gwei-tracker.js
+++ gwei-tracker.js
@@ -1,33 +1,47 @@
-const apiKey = 'BENNGHJVW5D83N7XWIET967RC3SBTDNNYY';
+const apiKey = 'BENNGHJVW5D83N7XWIET967RC3SBTDNNYY'
+const standardElement = document.getElementsByClassName('standard-gas-fee')[0]
+const fastElement = document.getElementsByClassName('fast-gas-fee')[0]
+const instantElement = document.getElementsByClassName('instant-gas-fee')[0]
+const setButton = document.getElementById('setButton')
 
-// Pobierz elementy HTML
-const standardElement = document.getElementsByClassName('standard-gas-fee')[0];
-const fastElement = document.getElementsByClassName('fast-gas-fee')[0];
-const instantElement = document.getElementsByClassName('instant-gas-fee')[0];
+let alertGasNumberInput
 
-// Funkcja do pobierania i aktualizacji cen gazu
-function fetchGasPrices() {
-    const apiUrl = `https://api.etherscan.io/api?module=gastracker&action=gasoracle&apikey=${apiKey}`;
-
-    fetch(apiUrl)
-        .then(response => response.json())
-        .then(data => {
-            if (data.status === '1') {
-                const standardGasPrice = data.result.SafeGasPrice;
-                const fastGasPrice = data.result.ProposeGasPrice;
-                const instantGasPrice = data.result.FastGasPrice;
-
-                // Przypisz pobrane ceny do odpowiednich elementów HTML
-                standardElement.textContent = standardGasPrice;
-                fastElement.textContent = fastGasPrice;
-                instantElement.textContent = instantGasPrice;
-            } else {
-                console.error('Błąd pobierania danych:', data.message);
-            }
-        })
-        .catch(error => console.error('Błąd pobierania danych:', error));
+function setAlertGasNumber() {
+	const gasAmountInput = document.getElementById('gasAmountInput')
+	if (gasAmountInput) {
+		alertGasNumberInput = gasAmountInput.value
+	}
 }
 
-// Uruchom funkcję na początku i powtarzaj co pewien czas (np. co 30 sekund)
-fetchGasPrices();
-setInterval(fetchGasPrices, 30000);  // 30 000 ms = 30 s
+function fetchGasPrices() {
+	const apiUrl = `https://api.etherscan.io/api?module=gastracker&action=gasoracle&apikey=${apiKey}`
+
+	fetch(apiUrl)
+		.then(response => response.json())
+		.then(data => {
+			if (data.status === '1') {
+				const { SafeGasPrice, ProposeGasPrice, FastGasPrice } = data.result
+
+				standardElement.textContent = SafeGasPrice
+				fastElement.textContent = ProposeGasPrice
+				instantElement.textContent = FastGasPrice
+
+				if (alertGasNumberInput && SafeGasPrice < alertGasNumberInput) {
+					startAlert()
+				}
+			} else {
+				console.error('Błąd pobierania danych:', data.message)
+			}
+		})
+		.catch(error => console.error('Błąd pobierania danych:', error))
+}
+
+function startAlert() {
+	const audio = new Audio('audio/alert.wav')
+	audio.play()
+}
+
+setButton.addEventListener('click', setAlertGasNumber)
+
+fetchGasPrices()
+setInterval(fetchGasPrices, 3000)
